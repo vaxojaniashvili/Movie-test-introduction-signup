84319ff46d080fe3a6b514712cd279c712fb5e8a
vaxo <vaxojaniashvili186@gmail.com>
2024-04-16 00:40:21 +0400

    updated-readme

---
+++ src/app/pages/Register/Sign-up/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="w-full">
+      <div className="flex text-white font-RegisterFont w-full justify-center">
+        <div className="py-20">
+          <img
+            className="rounded-full w-[32px] h-[25px]"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWQL_h_IpF_nnjVfbHBJxv8X29QkE4SfRB0v4R3O_zpA&s"
+          />
+        </div>
+      </div>
+      <div className="w-[400px] h-[418px] bg-[#161D2F] rounded-[1.25rem]">
+        <h1 className="text-white">Sign Up</h1>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
