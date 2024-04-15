9b06fb59c1c6cade7f66eab5e949880cec8403cd
vaxo <vaxojaniashvili186@gmail.com>
2024-04-16 03:01:09 +0400

    Registration-UI-finished

--- src/app/components/Sign-up-input/SignUpInput.tsx
+++ src/app/components/Sign-up-input/SignUpInput.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SignUpInput = ({type,placeholder,className,name,onChange,value}:any) => {
   return (
-    <input className={className} type={type} placeholder={placeholder} name={name} onChange={onChange} value={value} />
+    <input className={className} type={type} placeholder={placeholder} name={name} onChange={onChange} />
   )
 }
 
--- src/app/pages/Register/Sign-up/page.tsx
+++ src/app/pages/Register/Sign-up/page.tsx
@@ -1,10 +1,18 @@
-import SignUpSumInputs from "@/app/components/Sign-up-input/SignUpSumInputs";
+"use client";
+import SignUpInput from "@/app/components/Sign-up-input/SignUpInput";
 import Link from "next/link";
 import React, { useState } from "react";
 
 const SignUp = () => {
+  const [emailValue, setEmailValue] = useState("");
+
+  const handleSignUp = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log(emailValue);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSignUp}>
       <div className="w-full font-RegisterFont">
         <div className="flex text-white w-full justify-center">
           <div className="py-10 md:py-20">
@@ -17,10 +25,33 @@ const SignUp = () => {
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90%] md:w-[400px] h-[440px] bg-[#161D2F] px-8 py-8 rounded-[1.25rem]">
           <h1 className="text-[#fff] text-[32px] mb-6">Sign Up</h1>
           <div className="my-4 md:my-[40px]">
-            <SignUpSumInputs
-            emailClassName="border-b-2 border-[#5A698F] text-white placeholder:text-[15px] outline-none w-full py-2 px-5 bg-transparent text-[15px] mb-4"
-            passwordClassName={`border-b-2 border-[#5A698F] text-white placeholder:text-[15px] outline-none w-full py-2 px-5 bg-transparent text-[15px] mb-4`}
-            rePassClassName="border-b-2 border-[#5A698F] text-white placeholder:text-[15px] outline-none w-full py-2 px-5 bg-transparent text-[15px] mb-4" />
+            <SignUpInput
+              onChange={(e: {
+                target: { value: React.SetStateAction<string> };
+              }) => setEmailValue(e.target.value)}
+              className="border-b-2 border-[#5A698F] text-white placeholder:text-[15px] outline-none w-full py-2 px-5 bg-transparent text-[15px] mb-4"
+              type="email"
+              placeholder="Email address"
+              name="email"
+            />{" "}
+            <SignUpInput
+              onChange={(e: {
+                target: { value: React.SetStateAction<string> };
+              }) => setEmailValue(e.target.value)}
+              className="border-b-2 border-[#5A698F] text-white placeholder:text-[15px] outline-none w-full py-2 px-5 bg-transparent text-[15px] mb-4"
+              type="password"
+              placeholder="Password"
+              name="password"
+            />
+            <SignUpInput
+              onChange={(e: {
+                target: { value: React.SetStateAction<string> };
+              }) => setEmailValue(e.target.value)}
+              className="border-b-2 border-[#5A698F] text-white placeholder:text-[15px] outline-none w-full py-2 px-5 bg-transparent text-[15px] mb-4"
+              type="password"
+              placeholder="Repeat password"
+              name="re-password"
+            />
           </div>
           <button
             type="submit"
